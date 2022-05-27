1faa99593e0a2d3f90fc4d8438d38c409e8dfa0b
Om Shahane <71377782+shahane806@users.noreply.github.com>
2022-05-27 16:21:04 +0530

    d

--- Assignment 6/setC/que1.cpp
+++ Assignment 6/setC/que1.cpp
@@ -6,3 +6,56 @@ Write a C++ program to overload following operators
 < Returns 1 if first fraction is less than second fraction
 
 */
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class fraction
+{
+    private : 
+    float numerator , denominator;
+    public  :
+    void get(void)
+    {
+        cout<<"Enter numerator : ";
+        cin>>numerator;
+        cout<<"Enter denominator : ";
+        cin>>denominator;
+    }
+    void put(void)
+    {
+        cout<<"Numerator : "<<numerator<<endl
+            <<"Denominator : "<<denominator<<endl;
+    }
+    void operator ++()
+    {
+        numerator++;
+        denominator++;
+    }
+    void operator ++(int)
+    {
+        ++numerator;
+        ++denominator;
+    }
+    void operator <(fraction f)
+    {
+        if((numerator/denominator)<(f.numerator/f.denominator))
+        {
+            cout<<"f1 is less";
+        }
+        else
+        {
+            cout<<"f2 is less";
+        }
+    }
+};
+int main()
+{
+    fraction f1,f2;
+    f1.get();
+    f1.put();
+    f2.get();
+    f2.put();
+    f1<f2;
+    return 0;
+}
--- Assignment 6/setC/que2.cpp
+++ Assignment 6/setC/que2.cpp
@@ -1,7 +1,98 @@
 /*
 
-Create a class Rational to represent a Rational number. 
+Create a class Rational to represent a Rational number.
 Perform the Basic Arithmetic Operations:
 Addition, Subtraction, Multiplication and Division for two Rational Numbers.
 
 */
+#include <iostream>
+#include <conio.h>
+using namespace std;
+class Rational
+{
+private:
+    float numerator, denominator;
+
+public:
+    void get(void)
+    {
+        cout << "Enter numerator : ";
+        cin >> numerator;
+        cout << "Enter denominator : ";
+        cin >> denominator;
+    }
+    void put(void)
+    {
+        cout << "Numerator : " << numerator << endl
+             << "Denominator : " << denominator << endl;
+    }
+
+    Rational operator+(Rational f)
+    {
+        Rational temp;
+
+        temp.numerator = f.numerator;
+        temp.denominator = f.denominator;
+
+        f.numerator = f.numerator * denominator;
+        f.denominator = f.denominator * denominator;
+        denominator = denominator * temp.denominator;
+        numerator = numerator * temp.denominator;
+
+        temp.numerator = numerator + f.numerator;
+        temp.denominator = denominator;
+
+        return temp;
+    }
+    Rational operator-(Rational f)
+    {
+        Rational temp;
+
+        temp.numerator = f.numerator;
+        temp.denominator = f.denominator;
+
+        f.numerator = f.numerator * denominator;
+        f.denominator = f.denominator * denominator;
+        denominator = denominator * temp.denominator;
+        numerator = numerator * temp.denominator;
+
+        temp.numerator = numerator - f.numerator;
+        temp.denominator = denominator;
+
+        return temp;
+    }
+    Rational operator*(Rational f)
+    {
+        Rational temp;
+
+        temp.numerator = f.numerator * numerator;
+        temp.denominator = f.denominator * denominator;
+
+        return temp;
+    }
+    Rational operator /(Rational f)
+    {
+        Rational temp;
+        temp.numerator = f.numerator * denominator;
+        temp.denominator = f.denominator * numerator;
+        return temp;
+    }
+};
+
+int main()
+{
+    Rational r1, r2, r3, r4, r5, r6;
+    r1.get();
+    r1.put();
+    r2.get();
+    r2.put();
+    r3 = r1 + r2;
+    r3.put();
+    r4 = r1 - r2;
+    r4.put();
+    r5 = r1 * r2;
+    r5.put();
+    r6 = r1 / r2;
+    r6.put();
+    return 0;
+}
